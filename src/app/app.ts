951f57789b38d93c48d9b3ff5cb72641b03182ef
Bishes maharjan <bishescha2071@gmail.com>
2025-08-20 16:15:27 +0545

    Added field component, static

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/button/button';
 import { FuiInput } from "./components/fui-input/fui-input";
+import { FuiField } from "./fui-field/fui-field";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonComponent, FuiInput],
+  imports: [RouterOutlet, ButtonComponent, FuiInput, FuiField],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
